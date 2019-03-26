166567e9482361239f62c22df572d071a2179171
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-26 00:52:59 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   // Write your JavaScript code inside the init() function
   let index = 0;
-  
+
   function init() {
   document.body.addEventListener('keydown', function(e) {
     let key = parseInt(e.which || e.detail);
